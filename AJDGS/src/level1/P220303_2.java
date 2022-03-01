cab9465c31e279d432624ff23a7943c52fca11d4
rachaen <rachaenlee@gmail.com>
2022-03-01 20:12:13 +0900

    서울에서 김서방 찾기

---
+++ AJDGS/src/level1/P220303_2.java
@@ -0,0 +1,12 @@
+package AJDGS.src.level1;
+
+public class P220303_2 {
+    public String solution(String[] seoul) {
+        String answer = "";
+        for(int i = 0; i < seoul.length; i++) {
+            if(seoul[i].equals("Kim") )
+                answer = String.format("김서방은 %d에 있다", i);
+        }
+        return answer;
+    }
+}
