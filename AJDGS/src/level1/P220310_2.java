b6de45178f9d8e417c1e9b78795a3f43133886c1
rachaen <rachaenlee@gmail.com>
2022-03-05 23:30:02 +0900

    문자열을 정수로 바꾸기

---
+++ AJDGS/src/level1/P220310_2.java
@@ -0,0 +1,9 @@
+package AJDGS.src.level1;
+
+public class P220310_2 {
+    public int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+}
