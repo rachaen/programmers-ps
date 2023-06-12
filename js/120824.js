76fde666709f50b31791faf76898c2f60850612e
Chaeeun <78066837+rachaen@users.noreply.github.com>
2023-06-12 13:50:48 +0900

    짝수 홀수 개수

---
+++ js/120824.js
@@ -0,0 +1,9 @@
+function solution(num_list) {
+    let answer = [0, 0];
+    num_list.forEach((i) => {
+        if(i % 2 == 0) answer[0]++;
+        else answer[1]++;
+    })
+
+    return answer;
+}
